1635021861c47aaa64d0fef94ed979eb1f19ce3a
John Sisk <151465587+JohnSisk4@users.noreply.github.com>
2025-07-23 18:36:59 +0100

    Add files via upload

---
+++ ConsoleAppAssignment.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAssignment
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+//            // Boolean condition to check if a number is less than 5
+//            bool isLessThanFive = true;
+//            int counter = 0;
+
+//            // WHILE LOOP EXAMPLE
+//            // The loop will continue as long as isLessThanFive is true
+//            Console.WriteLine("Using while loop:");
+//            while (isLessThanFive)
+//            {
+//                // Print the current counter value
+//                Console.WriteLine("Counter is: " + counter);
+
+//                // Increment the counter
+//                counter++;
+
+//                // Once counter reaches 5, set the condition to false
+//                if (counter >= 5)
+//                {
+//                    isLessThanFive = false;
+//                }
+//            }
+
+//            // Reset values for the do...while loop example
+//            bool isGreaterThanZero = true;
+//            int number = 5;
+
+//            // DO WHILE LOOP EXAMPLE
+//            // This loop will execute at least once before checking the condition
+//            Console.WriteLine("\nUsing do...while loop:");
+//            do
+//            {
+//                // Print the current number value
+//                Console.WriteLine("Number is: " + number);
+
+//                // Decrement the number
+//                number--;
+
+//                // Once number reaches 0, set the condition to false
+//                if (number <= 0)
+//                {
+//                    isGreaterThanZero = false;
+//                }
+
+//            } while (isGreaterThanZero); // Check the condition after executing the loop body
+//        }
+//    }
+//}
+
