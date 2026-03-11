9a4c756fad24cdd4b3631c10c0bca59916c0c556
John Sisk <151465587+JohnSisk4@users.noreply.github.com>
2026-03-11 10:15:40 +0000

    Add files via upload

---
+++ StructAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    // Define a struct called Number
+    public struct Number
+    {
+        // Declare a property called Amount of type decimal
+        public decimal Amount { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of the Number struct
+            Number myNumber = new Number();
+
+            // Assign a decimal value to the Amount property
+            myNumber.Amount = 250.75m;
+
+            // Print the value of Amount to the console
+            Console.WriteLine("The amount is: " + myNumber.Amount);
+
+            // Keep the console window open until a key is pressed
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
+
